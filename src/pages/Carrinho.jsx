da635eae6f8f6d8dff9bec5fd69fadf59ee56f35
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-15 12:33:29 -0300

    Update Carrinho.jsx

--- src/pages/Carrinho.jsx
+++ src/pages/Carrinho.jsx
@@ -2,7 +2,14 @@ export default function Carrinho(){
   return (
     <div>
       <h2 className="text-2xl font-bold mb-2">Carrinho</h2>
-      <p>Esta é a página do carrinho. Funcionalidade de carrinho não implementada neste exercício.</p>
+      {cart.map(item => (
+        <div key={item.id}>
+          <p>{item.title}</p>
+          <p>Qtd: {item.quantity}</p>
+        </div>
+      ))}
+    </>
+  );
     </div>
   )
 }
