f82849a6b467430951f196b6523f7a3728ddb338
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-07-15 12:10:59 -0300

    Create script.js

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const ProductCard = document.elementById("ProductCard");
+const buttonOnClick = document.elementById("buttonOnClick");
+
+export default function funcaoButtonOnClick(funcaoCart, funcaoVoltar) {
+  const [ProductCard, setProdutoCard] = useState([])
+  const [buttonOnClick, setButtonOnClick] = useState([])
+  
+  return (
+    function funcaoCart() {
+      const card = element.addEventListener("ProductCard");
+}
+    
+    function funcaoVoltar() {
+    const buttonOnClick = element.addEventListener("buttonOnClick");
+}
+  )
+}
