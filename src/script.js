4f6c2b0d353f62e6e55325ba79e6a17805d22248
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-08-05 11:56:25 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,1 +1,22 @@
+const e-mail = document.getElementById("e-mail");
+const senha = document.getElementById("senha");
+const register = document.getElementById("register");
+const login = document.getElementById("login");
+const logout = document.getElementById("logout");
 
+function login(e-mail, senha) {
+  const login = document.getElementById("e-mail", "senha");
+  document.getLocalStorage(e-mail, senha);
+  document.setLocalStorage(login);
+}
+
+function logout(login) {
+  const logout = document.getElementById("login").exit;
+  document.setLocalStorage(logout);
+}
+
+function register(nome, e-mail, senha) {
+  const register = document.getElementById("nome", "e-mail", "senha");
+  document.getLocalStorage("nome", "e-mail", "senha");
+  const saved = document.setLocalStorage(register);
+}
