e3dddce88be483ce282d0a641fd3ac50cebf5201
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-08-05 11:20:56 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Perfil from './pages/Perfil'
 import Carrinho from './pages/Carrinho'
 import ProdutoDetalhe from './pages/ProdutoDetalhe'
 import { CartProvider } from './context/CartContext'
-import Pedido from './pages/Pedido'
+import Pedidos from './pages/Pedidos'
 
 function App() {
   return (
@@ -19,7 +19,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="carrinho" element={<Carrinho />} />
             <Route path="produto/:id" element={<ProdutoDetalhe />} />
-            <Route path="pedido" elemennt={<Pedido />} />
+            <Route path="pedidos" elemennt={<Pedidos />} />
             <Route path="perfil" element={<Perfil />} />
           </Route>
         </Routes>
