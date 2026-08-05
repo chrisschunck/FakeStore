fce01850331bbe7afef8ed386d2804040ff09f47
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-08-05 10:55:25 -0300

    Update Perfil.jsx

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -7,5 +7,13 @@ export default function Perfil(){
       <h2 className="text-2xl font-bold mb-2">Perfil</h2>
       <p>Digite seu e-mail ou telefone para iniciar sessão ou se não registre-se.</p>
     </div>
+    <div className="card">
+      <div className="gap-4 py-20">
+        <label for>e-mail ou telefone</label>
+        <input type="" placeholder="digite seu email">email</input>
+        <label for="">senha</label>
+        <input type="" className="" placeholder="digite sua senha">senha</input>
+      </div>
+    </div>
   )
 }
