b522ac311673455c02f697fd3a1339be19e23576
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-08-05 10:49:41 -0300

    Update Perfil.jsx

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -1,5 +1,8 @@
 export default function Conta(){
   return (
+    <div className="header-top"
+      <div className="text-black-700 container mx-auto py-1 px-4 bg-white text-sm">FakeStore Explorer</div>
+    </div>
     <div>
       <h2 className="text-2xl font-bold mb-2">Perfil</h2>
       <p>Digite seu e-mail ou telefone para iniciar sessão ou se não registre-se.</p>
