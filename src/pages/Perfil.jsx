8e0a48ceeb5cb093b6fe6d79cea1856b2e9daa2c
Christian Schunck de Almeida <christian.schunck10@gmail.com>
2026-08-05 10:46:16 -0300

    Update Perfil.jsx

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -2,7 +2,7 @@ export default function Conta(){
   return (
     <div>
       <h2 className="text-2xl font-bold mb-2">Perfil</h2>
-      <p>Esta é a página de perfil. Substitua por informações do usuário se desejar.</p>
+      <p>Digite seu e-mail ou telefone para iniciar sessão ou se não registre-se.</p>
     </div>
   )
 }
